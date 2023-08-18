7896b7d848ea212511961da29c63c7271195060f
LÊ NGỌC NHÂN <111173019+core-aliyah-44112@users.noreply.github.com>
2023-08-18 17:25:54 +0700

    Add project files.

---
+++ WebsiteBanHang/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebsiteBanhangOnlineEntities dbWeb = new WebsiteBanhangOnlineEntities();
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AccountAdmin(int id)
+        {
+            var objaccount = dbWeb.Users.Where(n => n.Id == id).FirstOrDefault();
+            return View(objaccount);
+        }
+    }
+}
---
+++ WebsiteBanHang/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class ProductController : Controller
+    {
+        WebsiteBanhangOnlineEntities objwebbanhangonlineEntities =new WebsiteBanhangOnlineEntities();
+        // GET: Product
+        public ActionResult Product(string currentFilter, int? page)
+        {
+            ViewBag.CurrentFilter = currentFilter;
+            //Số item trong 1 trang = 5
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            //Sắp xếp theo id sản phẩm , sản phẩm mới đưa lên đầu.
+            var lstProduct = objwebbanhangonlineEntities.Products.Where(n => n.PriceDiscount != null).ToList();
+            Session["countPagePro"] = lstProduct.Count;
+            return View(lstProduct.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult Detail(int Id)
+        {
+            var objProduct=objwebbanhangonlineEntities.Products.Where(n=>n.Id==Id).FirstOrDefault();
+            
+            return View(objProduct);
+        }
+    }
+}
